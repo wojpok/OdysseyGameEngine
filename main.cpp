df6293cf3484236d135906c63f6c0bb90a1cd38a
Xelonidas <mipokoj@gmail.com>
2021-07-14 13:40:58 +0200

    Psysics engine structure

--- main.cpp
+++ main.cpp
@@ -2,21 +2,64 @@
 #include "src/view-module/viewHidden.h"
 #include "src/odysseyGameEngine.h"
 #include "math2D.hpp"
+#include "physic.h"
 
-float playerSpeed = 2;
-view::shader *sh2;
+static std::vector<physic*> staticColliders = {};
+static std::vector<physic*> dynamicColliders = {};
+
+void physic::update() {
+	for(physic* pd : dynamicColliders) {
+		for(physic* ps : staticColliders) {
+			
+			dynamicCollider* dc = dynamic_cast<dynamicCollider*>(pd);
+			staticCollider* sc = dynamic_cast<staticCollider*>(ps);
+			
+			if(dc == NULL || sc == NULL)
+				return;
+			
+		}
+	}
+}
+
+
+vector2 gravity(0, -3);
+
+staticCollider::staticCollider() {
+	staticColliders.push_back(this);
+}
+
+void staticCollider::update() {
+	origin = vector2(oge::parent->transform[1], oge::parent->transform[2]);
+}
+
+	
+dynamicCollider::dynamicCollider() {
+	dynamicColliders.push_back(this);
+}
+
+void dynamicCollider::update() {	
+	
+	velocity += gravity * chr::deltaTime;
+	
+	oge::parent->transform[1] += velocity.y * chr::deltaTime;
+	oge::parent->transform[2] += velocity.x * chr::deltaTime;
+	
+	origin = vector2(oge::parent->transform[1], oge::parent->transform[2]);
+}
+
 
 class timer : public oge::component {
 public:
+	dynamicCollider* col = NULL;
+	
 	void update() override {
-		oge::parent->transform[3] += chr::deltaTime;
-		
-		if (glfwGetKey(view::window, GLFW_KEY_W ) == GLFW_PRESS){
-			oge::parent->transform[1] += chr::deltaTime;
+		if(col == NULL) {
+			col = oge::parent->getComponent<dynamicCollider>();
+			return;
 		}
 		
-		if (glfwGetKey(view::window, GLFW_KEY_S ) == GLFW_PRESS){
-			oge::parent->transform[1] -= chr::deltaTime;
+		if(glfwGetKey(view::window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+			col->velocity = vector2(0, 3);
 		}
 	}
 };
@@ -51,11 +94,31 @@ int main() {
 	unsigned int blackT = view::loadBMP_custom("black.bmp");
 	
 	view::shape* squareS = new view::shape(g_uv_buffer_data, 12, square, 18, 6);
-			
+	// ==========================================================
 	oge::gameObject* bg = oge::createNewGameObject();
 	bg->components.push_back(new view::rendererComponent(sh, squareS, whiteT));
 
+	dynamicCollider* col = new dynamicCollider();
+	col->velocity = vector2(0, 3);
+	col->size = vector2(0.125, 0.125);
+	
 	bg->components.push_back(new timer());
+	bg->components.push_back(col);
+	
+	// ==========================================================
+	oge::gameObject* bg2 = oge::createNewGameObject();
+	bg2->components.push_back(new view::rendererComponent(sh, squareS, whiteT));
+	
+	bg2->setPos(0, -1, 0);
+	
+	staticCollider* col2 = new staticCollider();
+	col2->size = vector2(0.125, 0.125);
+	
+	bg2->components.push_back(col2);
+	// ==========================================================
+	oge::gameObject* physicEngine = oge::createNewGameObject();
+	physicEngine->components.push_back(new physic());
+	
 	
 	oge::gameLoop();
 	
---
+++ physic.h
@@ -0,0 +1,25 @@
+#pragma once
+
+class physic : public oge::component {
+	void update() override;
+};
+
+class staticCollider : public physic {
+public:
+	vector2 size;
+	vector2 origin;
+	staticCollider();
+	void update() override;
+};
+
+class dynamicCollider : public physic {
+public:
+
+	vector2 velocity;	
+	vector2 size;
+	vector2 origin;
+	
+	dynamicCollider();
+	
+	void update() override;
+};
